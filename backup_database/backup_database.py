6a96e04156fe1e4cb2f63963ae840ee0840cae36
g-kimbell <grahamkimbell@outlook.com>
2024-05-28 12:40:04 +0200

    Initial commit

---
+++ backup_database/backup_database.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+from datetime import datetime
+import shutil
+
+DATABASE_FILEPATH = "C:\\Modules\\Database\\chemspeedDB.db"
+BACKUP_FOLDER = "C:\\Modules\\Database\\Backup"
+
+try:
+    with sqlite3.connect(DATABASE_FILEPATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT value FROM Settings_Table WHERE key = 'Base Sample ID'")
+        result = cursor.fetchone()
+        if result is not None:
+            value = result[0]
+        else:
+            value = ""
+except sqlite3.Error as e:
+    print("Database error: ", e)
+    value = ""
+
+if value == "":
+    value = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    print(f"Base Sample ID not found in the database. Using current timestamp instead.")
+
+# copy database file to backup folder with the base sample ID as the filename
+os.makedirs(BACKUP_FOLDER, exist_ok=True)
+backup_filepath = f"{BACKUP_FOLDER}\\{value}.db"
+shutil.copy(DATABASE_FILEPATH, backup_filepath)
+print(f"Database backed up to {backup_filepath}.")
