53c94d375d6b807e43f9da40eb7d5ec18647aa43
g-kimbell <grahamkimbell@outlook.com>
2024-08-23 13:40:52 +0200

    Change folder structure

---
+++ aurora_robot_tools/version.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.1'
+__author__ = 'Graham Kimbell'
+__title__ = 'Aurora robot tools'
+__description__ = """
+Scripts for the Aurora battery assembly robot at Empa.
+"""
+__license__ = 'MIT'
+__copyright__ = '2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia'
+__url__ = 'https://github.com/EmpaEconversion/aurora-robot-tools'
