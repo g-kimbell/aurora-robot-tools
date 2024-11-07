c0da92bb4078b74ec8714c3fdf166a48e3285f3c
g-kimbell <grahamkimbell@outlook.com>
2024-11-07 15:53:15 +0100

    Change N:P ratio overlap calculation, formatting
    N:P ratio now uses electrode diameters, not overlap factor
    
    Makes calculation possible when using different electrode diameters, not just 14 and 15

--- aurora_robot_tools/capacity_balance/capacity_balance.py
+++ aurora_robot_tools/capacity_balance/capacity_balance.py
@@ -1,4 +1,4 @@
-""" Copyright © 2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia
+"""Copyright © 2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia.
 
 Match cathodes with anodes to achieve the desired N:P ratio.
 
@@ -40,30 +40,32 @@ Usage:
                 If N:P ratios do not change, use 2D matching (method 3), otherwise try exact 3D
                 (method 5), if too slow use greedy 3D (method 4)
 
-TODO:
+Todo:
     - Make rejection_cost_factor an argument when AutoSuite supports it.
-    - [Long term] Pre-calculate the possible matchings using different rejection_cost_factors and 
+    - [Long term] Pre-calculate the possible matchings using different rejection_cost_factors and
       allow the user to choose the best one.
-"""
 
-import sys
-import sqlite3
+"""
 import itertools
+import sqlite3
+import sys
+
 import numpy as np
 import pandas as pd
-from scipy.optimize import linear_sum_assignment
 import pulp
+from scipy.optimize import linear_sum_assignment
 
 DATABASE_FILEPATH = "C:\\Modules\\Database\\chemspeedDB.db"
 
 TIMEOUT_SECONDS = 30
 
 
-def calculate_capacity(df):
+def calculate_capacity(df: pd.DataFrame) -> None:
     """Calculate the capacity of the anodes and cathodes in-place in the main dataframe, df.
 
     Args:
         df (pandas.DataFrame): The dataframe containing the cell assembly data.
+
     """
     for xode in ["Anode", "Cathode"]:
         df[f"{xode} Active Material Weight (mg)"] = (
@@ -76,12 +78,9 @@ def calculate_capacity(df):
         if (df[f"{xode} Balancing Capacity (mAh)"] < 0).any():
             print(f"WARNING: {xode} capacities below 0, setting to NaN")
             df.loc[df[f"{xode} Balancing Capacity (mAh)"] < 0, f"{xode} Balancing Capacity (mAh)"] = np.nan
-    # HACK not using actual diameters, since this doesn't work if electrode numbers are unequal
-    # The robot can only use 15 mm and 14 mm electrodes anyway
-    df["N:P ratio overlap factor"] = 14**2 / 15**2
 
 
-def cost_matrix_assign(df, rejection_cost_factor = 2):
+def cost_matrix_assign(df: pd.DataFrame, rejection_cost_factor: float = 2) -> tuple[list[int], list[int]]:
     """Calculate the cost matrix and find the optimal matching of anodes and cathodes.
 
     Args:
@@ -93,9 +92,13 @@ def cost_matrix_assign(df, rejection_cost_factor = 2):
 
     Returns:
         tuple: The indices of the optimal matching of anodes and cathodes.
+
     """
     # Calculate all possible N:P ratios
-    actual_ratio = np.outer(df["N:P ratio overlap factor"] * df["Anode Balancing Capacity (mAh)"], 1 / df["Cathode Balancing Capacity (mAh)"])
+    actual_ratio = np.outer(
+        df["Anode Balancing Capacity (mAh)"] / df["Anode Diameter (mm)"]**2,
+        1 / (df["Cathode Balancing Capacity (mAh)"] / df["Cathode Diameter (mm)"]**2),
+    )
     n = actual_ratio.shape[0]
 
     # Cells outside N:P ratio limits are rejected, given the same cost scaled by rejection_cost_factor
@@ -124,9 +127,11 @@ def cost_matrix_assign(df, rejection_cost_factor = 2):
     return anode_ind, cathode_ind
 
 
-def exact_npartite_matching(cost_matrix):
+def exact_npartite_matching(cost_matrix: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Find the optimal matching of anodes and cathodes using an exact 3D matching algorithm.
-    This algorithm his NP-hard and can take a very long time for n>10"""
+
+    This algorithm his NP-hard and can take a very long time for n>10
+    """
     # Get the size of the cost matrix
     n = cost_matrix.shape[0]
 
@@ -134,10 +139,10 @@ def exact_npartite_matching(cost_matrix):
     assignments = list(itertools.product(range(n), repeat=3))
 
     # Create a binary variable for each assignment
-    x = pulp.LpVariable.dicts('x', assignments, cat=pulp.LpBinary)
+    x = pulp.LpVariable.dicts("x", assignments, cat=pulp.LpBinary)
 
     # Create the problem
-    problem = pulp.LpProblem('3D_assignment', pulp.LpMinimize)
+    problem = pulp.LpProblem("3D_assignment", pulp.LpMinimize)
 
     # The objective is to minimize the total cost of the chosen assignments
     problem += pulp.lpSum(cost_matrix[a]*x[a] for a in assignments)
@@ -149,21 +154,22 @@ def exact_npartite_matching(cost_matrix):
         problem += pulp.lpSum(x[a] for a in assignments if a[2] == i) == 1
 
     # Solve the problem
-    print(f'Attempting exact matching, will give up if a solution not found in {TIMEOUT_SECONDS} seconds...')
-    problem.solve(pulp.PULP_CBC_CMD(options=[f'sec={TIMEOUT_SECONDS}'],msg=False))
-    if pulp.LpStatus[problem.status] != 'Optimal':
-        raise ValueError(f'Optimal solution not found. Status: {pulp.LpStatus[problem.status]}')
-    else:
-        print('Optimal solution found')
+    print(f"Attempting exact matching, will give up if a solution not found in {TIMEOUT_SECONDS} seconds...")
+    problem.solve(pulp.PULP_CBC_CMD(options=[f"sec={TIMEOUT_SECONDS}"],msg=False))
+    if pulp.LpStatus[problem.status] != "Optimal":
+        msg = f"Optimal solution not found. Status: {pulp.LpStatus[problem.status]}"
+        raise ValueError(msg)
+    print("Optimal solution found")
     # Get the optimal assignments
     optimal_assignments = np.array([a for a in assignments if pulp.value(x[a]) == 1])
     i_idx, j_idx, k_idx = optimal_assignments[:,0], optimal_assignments[:,1], optimal_assignments[:,2]
     return i_idx, j_idx, k_idx
 
 
-def greedy_npartite_matching(cost_matrix):
+def greedy_npartite_matching(cost_matrix: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Find the optimal matching of anodes, cathodes, and target ratios using a greedy algorithm.
-    This will find a suboptimal solution, but does not suffer from combinatoral explosion like the 
+
+    This will find a suboptimal solution, but does not suffer from combinatoral explosion like the
     exact method.
     """
     # Get the shape of the cost matrix
@@ -193,9 +199,12 @@ def greedy_npartite_matching(cost_matrix):
     return i_idx, j_idx, k_idx
 
 
-def cost_matrix_assign_3d(df, rejection_cost_factor = 2 , exact=False):
-    """Calculate the cost matrix and find the optimal matching of anodes, cathodes, and target
-    ratios using a 3D matching algorithm.
+def cost_matrix_assign_3d(
+        df: pd.DataFrame,
+        rejection_cost_factor: float = 2,
+        exact: bool = False,
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Calculate the cost matrix and find optimal matching with 3D algorithm.
 
     Args:
         df (pandas.DataFrame): The dataframe containing the cell assembly data.
@@ -203,16 +212,18 @@ def cost_matrix_assign_3d(df, rejection_cost_factor = 2 , exact=False):
             1 = no extra cost for rejecting, more rejected cells, better N:P ratio of accepted cells
             10 = high cost to reject cells, fewer rejected cells, worse N:P ratio of accepted cells
             2 = compromise
+        exact (bool, optional): Use exact matching. Defaults to False.
 
     Returns:
         tuple: The indices of the optimal matching of anodes and cathodes.
+
     """
     n = len(df)
 
     # Convert all 1D arrays to 3D n x n x n arrays
-    anode_capacity = np.array(df["N:P ratio overlap factor"] * df["Anode Balancing Capacity (mAh)"])
+    anode_capacity = np.array(df["Anode Balancing Capacity (mAh)"]/df["Anode Diameter (mm)"]**2)
     anode_capacity = np.tile(anode_capacity[:, np.newaxis, np.newaxis], (1, n, n))
-    cathode_capacity = np.array(df["Cathode Balancing Capacity (mAh)"])
+    cathode_capacity = np.array(df["Cathode Balancing Capacity (mAh)"]/df["Cathode Diameter (mm)"]**2)
     cathode_capacity = np.tile(cathode_capacity[np.newaxis, :, np.newaxis], (n, 1, n))
     target_ratio = np.array(df["Target N:P Ratio"])
     target_ratio = np.tile(target_ratio[np.newaxis, np.newaxis, :], (n, n, 1))
@@ -252,9 +263,14 @@ def cost_matrix_assign_3d(df, rejection_cost_factor = 2 , exact=False):
     return anode_ind[ind_sort], cathode_ind[ind_sort], ratio_ind[ind_sort]
 
 
-def rearrange_electrode_columns(df, row_indices, anode_ind, cathode_ind, ratio_ind):
-    """Rearrange the anode and cathode columns in-place in the main dataframe, df, 
-    based on the indices of the optimal matching.
+def rearrange_electrode_columns(
+        df: pd.DataFrame,
+        row_indices: np.ndarray,
+        anode_ind: np.ndarray,
+        cathode_ind: np.ndarray,
+        ratio_ind: np.ndarray,
+    ) -> None:
+    """Rearrange eletrode columns in-place in the main dataframe.
 
     Args:
         df (pandas.DataFrame): The dataframe containing the cell assembly data.
@@ -262,43 +278,51 @@ def rearrange_electrode_columns(df, row_indices, anode_ind, cathode_ind, ratio_i
         anode_ind (numpy.ndarray): Anode indices for optimal matching.
         cathode_ind (numpy.ndarray): Cathode indices for optimal matching.
         ratio_ind (numpy.ndarray): Ratio indices for optimal matching.
+
     """
-    anode_columns = [col for col in df.columns if 'Anode' in col]
-    cathode_columns = [col for col in df.columns if 'Cathode' in col]
-    ratio_columns = ['Target N:P Ratio', 'Minimum N:P Ratio', 'Maximum N:P Ratio']
+    anode_columns = [col for col in df.columns if "Anode" in col]
+    cathode_columns = [col for col in df.columns if "Cathode" in col]
+    ratio_columns = ["Target N:P Ratio", "Minimum N:P Ratio", "Maximum N:P Ratio"]
     df_immutable = df.copy()
     for column in anode_columns:
-        df.loc[row_indices, column] = df_immutable.loc[row_indices[anode_ind], column].values
+        df.loc[row_indices, column] = df_immutable.loc[row_indices[anode_ind], column].to_numpy()
     for column in cathode_columns:
-        df.loc[row_indices, column] = df_immutable.loc[row_indices[cathode_ind], column].values
+        df.loc[row_indices, column] = df_immutable.loc[row_indices[cathode_ind], column].to_numpy()
     for column in ratio_columns:
-        df.loc[row_indices, column] = df_immutable.loc[row_indices[ratio_ind], column].values
+        df.loc[row_indices, column] = df_immutable.loc[row_indices[ratio_ind], column].to_numpy()
+    # Recalculate N:P ratio overlap factor
+    df["N:P ratio overlap factor"] = (df["Cathode Diameter (mm)"]**2 / df["Anode Diameter (mm)"]**2).fillna(0)
 
 
-def update_cell_numbers(df, base_sample_id, check_NP_ratio=True):
+def update_cell_numbers(df: pd.DataFrame, base_sample_id: str, check_NP_ratio: bool = True) -> None:
     """Update the cell numbers in the main dataframe, df, based on the accepted cells.
-    
+
     Args:
         df (pandas.DataFrame): The dataframe containing the cell assembly data.
+        base_sample_id (str): The run ID for the cells.
+        check_NP_ratio (bool, optional): Check the N:P ratio. Defaults to True.
     """
     if check_NP_ratio:
-        df["Actual N:P Ratio"] = df["N:P ratio overlap factor"] * df["Anode Balancing Capacity (mAh)"] / df["Cathode Balancing Capacity (mAh)"]
+        df["Actual N:P Ratio"] = (
+            (df["Anode Balancing Capacity (mAh)"] / df["Anode Diameter (mm)"]**2) /
+            (df["Cathode Balancing Capacity (mAh)"] / df["Cathode Diameter (mm)"]**2)
+        )
         cell_meets_criteria = ((df["Actual N:P Ratio"] >= df["Minimum N:P Ratio"])
                                 & (df["Actual N:P Ratio"] <= df["Maximum N:P Ratio"]))
         accepted_cell_indices = np.where(cell_meets_criteria)[0]
-        rejected_cell_indices = np.where(~cell_meets_criteria & ~df["Actual N:P Ratio"].isnull())[0]
+        rejected_cell_indices = np.where(~cell_meets_criteria & ~df["Actual N:P Ratio"].isna())[0]
         average_deviation = np.mean(np.abs(df["Actual N:P Ratio"][accepted_cell_indices]
                                         - df["Target N:P Ratio"][accepted_cell_indices]))
-        print(f'Accepted {len(accepted_cell_indices)} cells '
-            f'with average N:P deviation from target: {average_deviation:.4f}\n'
-            f'Rejected {len(rejected_cell_indices)} cells.')
+        print(f"Accepted {len(accepted_cell_indices)} cells "
+            f"with average N:P deviation from target: {average_deviation:.4f}\n"
+            f"Rejected {len(rejected_cell_indices)} cells.")
     else:
         # accept any cell with an anode and cathode
         accepted_cell_indices = np.where(
-            ~df["Anode Type"].isnull() &
-            ~df["Cathode Type"].isnull()
+            ~df["Anode Type"].isna() &
+            ~df["Cathode Type"].isna(),
         )[0]
-        print(f'Accepted {len(accepted_cell_indices)} cells without checking N:P ratio.')
+        print(f"Accepted {len(accepted_cell_indices)} cells without checking N:P ratio.")
 
     # Re-write the Cell Number column to only include cells with both anode and cathode
     df["Cell Number"] = 0
@@ -307,28 +331,17 @@ def update_cell_numbers(df, base_sample_id, check_NP_ratio=True):
         df.loc[cell_index, "Sample ID"] = f"{base_sample_id}_{cell_number + 1:02d}"
 
 
-def main():
-    """Read the cell assembly data from the database, calculate the capacity of the anodes and
+def main() -> None:
+    """Full function to match cathodes with anodes and update the database.
+
+    Read the cell assembly data from the database, calculate the capacity of the anodes and
     cathodes, and match the cathodes with the anodes to achieve the desired N:P ratio. Write the
     updated table back to the database.
     """
-    if len(sys.argv) >= 2:
-        sorting_method = int(sys.argv[1])
-    else:
-        sorting_method = 6
+    sorting_method = int(sys.argv[1]) if len(sys.argv) >= 2 else 6
 
-    sorting_methods = {
-        0: "Do not sort, do not check N:P ratio",
-        1: "Do not sort",
-        2: "Sort by capacity",
-        3: "2D cost matrix",
-        4: "Greedy 3D matching",
-        5: "Exact 3D matching",
-        6: "Auto"
-    }
-
-    print(f'Reading from database {DATABASE_FILEPATH}')
-    print(f'Using sorting method: {sorting_methods[sorting_method]}')
+    print(f"Reading from database {DATABASE_FILEPATH}")
+    print(f"Using sorting method {sorting_method}")
 
     # Connect to the database and create the Cell_Assembly_Table
     with sqlite3.connect(DATABASE_FILEPATH) as conn:
@@ -393,7 +406,7 @@ def main():
                     try:
                         anode_ind, cathode_ind, ratio_ind = cost_matrix_assign_3d(df_batch,exact=True)
                     except ValueError:
-                        print('Exact matching took too long, using greedy matching instead')
+                        print("Exact matching took too long, using greedy matching instead")
                         anode_ind, cathode_ind, ratio_ind = cost_matrix_assign_3d(df_batch)
 
                 case 6: # Choose automatically
@@ -408,7 +421,7 @@ def main():
                         try:
                             anode_ind, cathode_ind, ratio_ind = cost_matrix_assign_3d(df_batch,exact=True)
                         except ValueError:
-                            print('Exact matching took too long, using greedy matching instead')
+                            print("Exact matching took too long, using greedy matching instead")
                             anode_ind, cathode_ind, ratio_ind = cost_matrix_assign_3d(df_batch)
 
             # Rearrange the electrodes in the main dataframe
@@ -416,7 +429,7 @@ def main():
 
         # Read base_sample_id from the settings table
         df_settings = pd.read_sql("SELECT * FROM Settings_Table", conn)
-        base_sample_id = df_settings.loc[df_settings["key"] == "Base Sample ID", "value"].values[0]
+        base_sample_id = df_settings.loc[df_settings["key"] == "Base Sample ID", "value"].to_numpy()[0]
 
         # Update the actual N:P ratio, accepted cell numbers and sample ID in the main dataframe
         if sorting_method == 0:
@@ -426,7 +439,7 @@ def main():
 
         # Write the updated table back to the database
         df.to_sql("Cell_Assembly_Table", conn, index=False, if_exists="replace")
-        print('Updated database successfully')
+        print("Updated database successfully")
 
 if __name__ == "__main__":
     main()
