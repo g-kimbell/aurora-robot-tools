b01beeb7c9c97baa2dbe0193c1b8eae545a18a63
g-kimbell <grahamkimbell@outlook.com>
2024-11-07 16:01:40 +0100

    Ruff formatting

--- aurora_robot_tools/backup_database/backup_database.py
+++ aurora_robot_tools/backup_database/backup_database.py
@@ -1,13 +1,13 @@
-""" Copyright © 2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia
+"""Copyright © 2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia.
 
 Script to backup the chemspeedDB database to a folder with the base sample ID as the filename.
 
 """
 
 import os
+import shutil
 import sqlite3
 from datetime import datetime
-import shutil
 
 DATABASE_FILEPATH = "C:\\Modules\\Database\\chemspeedDB.db"
 BACKUP_FOLDER = "C:\\Modules\\Database\\Backup"
@@ -17,17 +17,14 @@ try:
         cursor = conn.cursor()
         cursor.execute("SELECT value FROM Settings_Table WHERE key = 'Base Sample ID'")
         result = cursor.fetchone()
-        if result is not None:
-            value = result[0]
-        else:
-            value = ""
+        value = result[0] if result is not None else ""
 except sqlite3.Error as e:
     print("Database error: ", e)
     value = ""
 
 if value == "":
     value = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    print(f"Base Sample ID not found in the database. Using current timestamp instead.")
+    print("Base Sample ID not found in the database. Using current timestamp instead.")
 
 # copy database file to backup folder with the base sample ID as the filename
 os.makedirs(BACKUP_FOLDER, exist_ok=True)
