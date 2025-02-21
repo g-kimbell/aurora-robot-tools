b0ff56aa029ac307e04d446cfda538096251d817
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-02-21 12:40:06 +0100

    Create step definition dict

---
+++ aurora_robot_tools/step_definition.py
@@ -0,0 +1,60 @@
+"""Definition of steps in the battery assembly process."""
+
+step_definition = {
+    10: {
+        "Step": "Bottom",
+        "Description": "Place the bottom casing",
+    },
+    20: {
+        "Step": "Spacer",
+        "Description": "Place the bottom spacer",
+    },
+    30: {
+        "Step": "Anode",
+        "Description": "Place the anode face up",
+    },
+    40: {
+        "Step": "Cathode",
+        "Description": "Place the cathode face up",
+    },
+    50: {
+        "Step": "Electrolyte",
+        "Description": "Add the electrolyte before the separator",
+    },
+    60: {
+        "Step": "Separator",
+        "Description": "Place the separator",
+    },
+    70: {
+        "Step": "Electrolyte",
+        "Description": "Add the electrolyte after the separator",
+    },
+    80: {
+        "Step": "Anode",
+        "Description": "Place the anode face down",
+    },
+    90: {
+        "Step": "Cathode",
+        "Description": "Place the cathode face down",
+    },
+    100: {
+        "Step": "Spacer",
+        "Description": "Place the top spacer",
+    },
+    110: {
+        "Step": "Spring",
+        "Description": "Place the spring",
+    },
+    120: {
+        "Step": "Top",
+        "Description": "Place the top casing",
+    },
+    130: {
+        "Step": "Press",
+        "Description": "Press the cell using 7.8 kN hydraulic press",
+    },
+    140: {
+        "Step": "Return",
+        "Description": "Return the completed cell to the rack",
+    },
+}
