bf01943247f4085a5c02433a26ce4b8ad7485ce2
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-02-21 10:41:51 +0100

    Bump version

--- aurora_robot_tools/version.py
+++ aurora_robot_tools/version.py
@@ -1,9 +1,9 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Graham Kimbell'
 __title__ = 'Aurora robot tools'
 __description__ = """
 Scripts for the Aurora battery assembly robot at Empa.
 """
 __license__ = 'MIT'
-__copyright__ = '2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia'
+__copyright__ = '2025, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia'
 __url__ = 'https://github.com/EmpaEconversion/aurora-robot-tools'
