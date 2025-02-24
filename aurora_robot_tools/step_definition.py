fd6210b0cfcc88239159731988e0b75deced8cea
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-02-24 09:30:42 +0100

    Shorten descriptions, add old recipe

--- aurora_robot_tools/step_definition.py
+++ aurora_robot_tools/step_definition.py
@@ -1,60 +1,109 @@
 """Definition of steps in the battery assembly process."""
 
+# Current step definitions
 step_definition = {
     10: {
         "Step": "Bottom",
-        "Description": "Place the bottom casing",
+        "Description": "Place bottom casing",
     },
     20: {
         "Step": "Spacer",
-        "Description": "Place the bottom spacer",
+        "Description": "Place bottom spacer",
     },
     30: {
         "Step": "Anode",
-        "Description": "Place the anode face up",
+        "Description": "Place anode face up",
     },
     40: {
         "Step": "Cathode",
-        "Description": "Place the cathode face up",
+        "Description": "Place cathode face up",
     },
     50: {
         "Step": "Electrolyte",
-        "Description": "Add the electrolyte before the separator",
+        "Description": "Add electrolyte before separator",
     },
     60: {
         "Step": "Separator",
-        "Description": "Place the separator",
+        "Description": "Place separator",
     },
     70: {
         "Step": "Electrolyte",
-        "Description": "Add the electrolyte after the separator",
+        "Description": "Add electrolyte after separator",
     },
     80: {
         "Step": "Anode",
-        "Description": "Place the anode face down",
+        "Description": "Place anode face down",
     },
     90: {
         "Step": "Cathode",
-        "Description": "Place the cathode face down",
+        "Description": "Place cathode face down",
     },
     100: {
         "Step": "Spacer",
-        "Description": "Place the top spacer",
+        "Description": "Place top spacer",
     },
     110: {
         "Step": "Spring",
-        "Description": "Place the spring",
+        "Description": "Place spring",
     },
     120: {
         "Step": "Top",
-        "Description": "Place the top casing",
+        "Description": "Place top casing",
     },
     130: {
         "Step": "Press",
-        "Description": "Press the cell using 7.8 kN hydraulic press",
+        "Description": "Press cell using 7.8 kN hydraulic press",
     },
     140: {
         "Step": "Return",
-        "Description": "Return the completed cell to the rack",
+        "Description": "Return completed cell to rack",
+    },
+}
+
+# Step definitions from robot tools 0.1.x
+step_definition_0_1 = {
+    1: {
+        "Step": "Bottom",
+        "Description": "Place bottom casing",
+    },
+    2: {
+        "Step": "Anode",
+        "Description": "Place anode face up",
+    },
+    3: {
+        "Step": "Electrolyte",
+        "Description": "Add electrolyte before separator",
+    },
+    4: {
+        "Step": "Separator",
+        "Description": "Place separator",
+    },
+    5: {
+        "Step": "Electrolyte",
+        "Description": "Add electrolyte after separator",
+    },
+    6: {
+        "Step": "Cathode",
+        "Description": "Place cathode face down",
+    },
+    7: {
+        "Step": "Spacer",
+        "Description": "Place top spacer",
+    },
+    8: {
+        "Step": "Spring",
+        "Description": "Place spring",
+    },
+    9: {
+        "Step": "Top",
+        "Description": "Place top casing",
+    },
+    10: {
+        "Step": "Press",
+        "Description": "Press cell using 7.8 kN hydraulic press",
+    },
+    11: {
+        "Step": "Return",
+        "Description": "Return completed cell to rack",
     },
 }
