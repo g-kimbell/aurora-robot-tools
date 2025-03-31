56d70debd781914fb3bfdff83e27a5696d74d12b
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-03-31 14:01:08 +0200

    Show target on live feed

--- aurora_robot_tools/camera/camera_daemon.py
+++ aurora_robot_tools/camera/camera_daemon.py
@@ -11,6 +11,18 @@ from aurora_robot_tools.config import CAMERA_PORT, DATABASE_FILEPATH
 
 PHOTO_PATH = Path("C:/Aurora_webcam_images/")
 
+step_radius = {
+    1: 10,
+    2: 10,
+    30: 7.5,
+    40: 7,
+    60: 8,
+    80: 7.5,
+    90: 7,
+    100: 8,
+    120: 9.5,
+}
+radius_mm = 10
 
 def socket_listener() -> None:
     """Listen for capture message on socket."""
@@ -36,6 +48,8 @@ def socket_listener() -> None:
                 )
                 result = cursor.fetchone()
                 label = f"cell_{result[0]}_step_{result[1]}"
+            global radius_mm
+            radius_mm = step_radius.get(int(result[1]),10)
             photo_path = PHOTO_PATH / run_id / f"{label!s}.png"
             if not photo_path.parent.exists():
                 photo_path.parent.mkdir(parents=True)
@@ -48,6 +62,7 @@ def socket_listener() -> None:
 def main() -> None:
     """Start webcam, show in window, listen for capture command."""
     global latest_frame
+    global radius_mm
     print("Starting webcam, press q to quit.")
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use DirectShow
     latest_frame = None  # Global variable to store the latest frame
@@ -59,8 +74,10 @@ def main() -> None:
         if not ret:
             break
 
-        latest_frame = frame  # Update the latest frame
-
+        latest_frame = frame.copy()  # Update the latest frame
+        frame = cv2.circle(frame, (320,240), int(radius_mm*375/20), (0,0,255))
+        frame = cv2.line(frame,(320,0),(320,480), (0,0,255))
+        frame = cv2.line(frame,(0,240),(640,240), (0,0,255))
         cv2.imshow("Webcam Feed", frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
